a5e0813a78b07bec40a235b83de33d0fb0d239ee
Goldenfight <108311910+Goldenfight@users.noreply.github.com>
2022-09-02 10:09:58 -0300

    Update project1.rb

--- project1.rb
+++ project1.rb
@@ -1,3 +1,11 @@
 puts "-------------------------------------------"
 puts " | Seja Bem Vindo ao Jogo da Adivinhação | "
 puts "-------------------------------------------"
+puts "\n"
+
+puts "Qual seu nome ?"
+nome = gets
+puts "\n"
+
+puts "Seja Bem Vindo"+ nome
+
